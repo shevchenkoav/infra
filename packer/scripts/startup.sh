006009e6a61afdeb05a11962255cb399971f1c5c
Aleksandr Shevchenko <shevchenkoavl@gmail.com>
2017-10-04 18:18:15 +0300

    update files

--- packer/scripts/startup.sh
+++ packer/scripts/startup.sh
@@ -2,7 +2,6 @@
 #
 # install&deploy https://github.com/Artemmkin/reddit
 
-
 curl -Ssl https://raw.githubusercontent.com/shevchenkoav/infra/base-os-packer/packer/scripts/install_ruby.sh \
 	| sudo -u appuser -i bash
 curl -Ssl https://raw.githubusercontent.com/shevchenkoav/infra/base-os-packer/packer/scripts/install_mongodb.sh \
