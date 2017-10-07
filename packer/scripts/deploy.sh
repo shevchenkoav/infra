ee3ae6e1047db4e7c166c0b47153869408c860fc
Aleksandr Shevchenko <shevchenkoavl@gmail.com>
2017-10-07 19:48:25 +0300

    update deploy.sh

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 #
 # deploy reddit app
+
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit && bundle install
-systemctl enable puma.service
+wget https://raw.githubusercontent.com/shevchenkoav/infra/base-os-packer/packer/files/puma.service
+
+# cp puma.service /etc/systemd/system
+# rm puma.service
+# systemctl enable puma.service
 # puma -d
