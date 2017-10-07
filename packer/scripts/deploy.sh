a82a53901f6141b296d23e1e32c63a7aa47e7aed
Aleksandr Shevchenko <shevchenkoavl@gmail.com>
2017-10-07 22:39:54 +0300

    + sudo

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -5,7 +5,7 @@
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit && bundle install
 cd /etc/systemd/system
-curl -Ssl https://github.com/shevchenkoav/infra/blob/base-os-packer/packer/files/puma.service > puma.service
+sudo curl -Ssl https://github.com/shevchenkoav/infra/blob/base-os-packer/packer/files/puma.service > puma.service
 sudo systemctl enable puma.service
 
 # cp puma.service /etc/systemd/system
