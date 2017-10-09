87a11bbe43a7d22e1ddeefc9cd5c3d49706b1e75
Aleksandr Shevchenko <shevchenkoavl@gmail.com>
2017-10-09 21:54:00 +0300

    finish modified vars* & deploy.sh for deploy app and enable all services

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 #
-# deploy reddit app and enable puma service
+# deploy reddit app
 
 git clone https://github.com/shevchenkoav/reddit.git
 cd reddit && bundle install
+
+# enable puma service
 sudo curl -Ssl https://github.com/shevchenkoav/infra/blob/base-os-packer/packer/files/puma.service > puma.service
 sudo cp puma.service /etc/systemd/system
 sudo systemctl start puma
