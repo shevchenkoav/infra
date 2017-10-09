1b17443430d53611591532503c617e2935dbd934
Aleksandr <shevchenkoavl@gmail.com>
2017-10-10 01:24:16 +0300

    added sudo

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Install MongoDB
+# Install MongoDB.
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
