0b06952f09717b57b58b8f981e34a817f27db90a
Aleksandr Shevchenko <shevchenkoavl@gmail.com>
2017-10-09 21:24:24 +0300

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,16 @@ provider "google" {
   project = "terraform-project-182418"
   region = "europe-west1"
 }
+
+resource "google_compute_instance" "app" {
+  name          = "reddit-app"
+  machine_type  = "g1-small"
+  zone          = "europe-west1-b"
+
+  # Определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "reddit-base-"
+    }
+  }
+}
