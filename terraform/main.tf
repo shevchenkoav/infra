86caa2181779dc08e1db2dbc1c91134677a87548
Aleksandr <shevchenkoavl@gmail.com>
2017-10-13 20:09:39 +0300

    fix version google provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,26 @@
 provider "google" {
   project = "terraform-project-182418"
   region = "europe-west1"
+  version = "0.1.3"
 }
-
 resource "google_compute_instance" "app" {
   name          = "reddit-app"
   machine_type  = "g1-small"
   zone          = "europe-west1-b"
-
   # Определение загрузочного диска
   boot_disk {
     initialize_params {
-      image = "reddit-base-"
+      image = "reddit-base-1507724278"
     }
   }
+  # определение сетевого интерфейса
+  network_interface {
+
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
 }
