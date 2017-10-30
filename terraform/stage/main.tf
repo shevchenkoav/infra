9c78f62c2aa3bac6062406a6a15bb349aa1a4bfc
Aleksandr <shevchenkoavl@gmail.com>
2017-10-30 12:02:27 +0300

    aplication add

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -16,11 +16,12 @@ terraform {
 
 # [modules] terraform get
 
-module "app" {
+module "application" {
   source              = "../modules/app"
   public_key_path     = "${var.public_key_path}"
   app_disk_image      = "${var.app_disk_image}"
 }
+
 
 module "db" {
   source              = "../modules/db"
