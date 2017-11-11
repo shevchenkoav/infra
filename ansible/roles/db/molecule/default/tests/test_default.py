260c3cc81de4a6811cefda8ea08d0fa9d41c9498
Aleksandr <shevchenkoavl@gmail.com>
2017-11-11 23:13:57 +0300

    update readme

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -18,8 +18,15 @@ def test_mongo_running_and_enabled(host):
     mongo = host.service("mongod")
     assert mongo.is_running
     assert mongo.is_enabled
+
 # check if configuration file contains the required line
 def test_config_file(File):
     config_file = File('/etc/mongod.conf')
+    assert config_file.contains('port: 27017')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+# check if mongod process is listening on localhost
+def test_mongod_is_listening(host):
+    port = host.socket('tcp://127.0.0.1:27017')
+    assert port.is_listening
